712554c20bc53866118f1f26adea7392d6c3b000
sufyanamir <sufi.canola@gmail.com>
2023-11-16 12:34:21 +0500

    Made branches module.

---
+++ app/Models/CompanyBranch.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CompanyBranch extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'company_branches';
+
+    protected $primaryKey = 'branch_id';
+
+    protected $fillable = [
+        'company_id',
+        'branch_code',
+        'branch_name',
+        'branch_email',
+        'branch_phone',
+        'branch_address',
+        'branch_manager',
+    ];
+
+    public $timestamps = true;
+}
---
+++ database/migrations/2023_11_16_064339_branch_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_branches', function(Blueprint $table){
+            $table->id('branch_id');
+            $table->integer('company_id');
+            $table->text('branch_code')->unique();
+            $table->text('branch_name');
+            $table->text('branch_email');
+            $table->double('branch_phone');
+            $table->text('branch_address');
+            $table->string('branch_manager');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable()->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company_branches');
+    }
+};
