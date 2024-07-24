3c843eddd4eba0ea3ececb0f433041974d763cba
sufyanamir <sufi.canola@gmail.com>
2024-07-24 10:47:21 +0500

    added websockets logic using the laravel websocket package.

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,11 +36,12 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
-                'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true,
+                'host' => env('LARAVEL_WEBSOCKETS_HOST', '127.0.0.1'),
+                'port' => env('LARAVEL_WEBSOCKETS_PORT', 6001),
+                'scheme' => env('LARAVEL_WEBSOCKETS_SCHEME', 'http'),
+                'encrypted' => env('LARAVEL_WEBSOCKETS_ENCRYPTED', false),
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
