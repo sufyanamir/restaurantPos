21ab9de4bda452dfe8e5f738dcf3d9f903d55492
sufyanamir <sufi.canola@gmail.com>
2025-03-19 16:33:55 +0500

    made change in attendance model

--- app/Models/StaffAttendance.php
+++ app/Models/StaffAttendance.php
@@ -23,4 +23,19 @@ class StaffAttendance extends Model
 
     public $timestamps = true;
 
+    public function company()
+    {
+        return $this->belongsTo(Company::class, 'company_id');
+    }
+
+    public function branch()
+    {
+        return $this->belongsTo(CompanyBranch::class, 'branch_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'added_user_id');
+    }
+
 }
